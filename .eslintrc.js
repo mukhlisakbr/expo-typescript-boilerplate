0252c91283d49d96ccd4cbd93fecd5a72e3bd489
mukhlisakbr <mukhlis3389@gmail.com>
2021-12-24 13:26:55 +0700

    chore(*): add prettier to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,9 @@ module.exports = {
   },
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
 };
