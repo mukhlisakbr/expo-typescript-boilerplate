8a51ec3ce072d2d3129a80b4ea7cacd86117ae8c
mukhlisakbr <mukhlis3389@gmail.com>
2021-12-25 13:40:43 +0700

    feat(*): add nativebase [skip release]

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { store } from './src/store';
 import { Provider } from 'react-redux';
 import { BottomTabs } from './src/navigation/bottom-tabs';
+import { NativeBaseProvider } from 'native-base';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <BottomTabs />
+      <NativeBaseProvider>
+        <BottomTabs />
+      </NativeBaseProvider>
     </Provider>
   );
 }
