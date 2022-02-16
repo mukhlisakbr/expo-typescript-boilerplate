60e908aecd6f45e67fce7a4e80400b9157344089
Mukhlis Akbarrudin <mukhlis3389@gmail.com>
2022-02-16 20:17:30 +0700

    feat: add standard-version

---
+++ .versionrc.js
@@ -0,0 +1,20 @@
+// .versionrc.js
+module.exports = {
+  bumpFiles: [
+    {
+      filename: 'package.json',
+    },
+    {
+      filename: 'app.json',
+      updater: require.resolve('standard-version-expo'),
+    },
+    {
+      filename: 'app.json',
+      updater: require.resolve('standard-version-expo/android'),
+    },
+    {
+      filename: 'app.json',
+      updater: require.resolve('standard-version-expo/ios'),
+    },
+  ],
+};
